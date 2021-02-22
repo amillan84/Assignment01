6cd5a4df0a6991c9242e08b10f91084d10ae54cf
Aida Millan <amillan84@gmail.com>
2021-02-21 20:18:10 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,131 @@
+//Step 1 Converting to camelCase
+//let someMonth;
+//theMonth = function(){};
+//let currentMonth;
+//let summerMonth;
+//let myLibraryFunction;
+
+//Step 2 Give example of a numerical, a string, a boolean, and a null literal expression
+// "Sunny day today." //string literal expression
+// "22" // numerical literal expression
+// true // Boolean literal expression
+// null // null literal expression
+
+//Step 3 Give 2 examples of complex / variable expressions
+// let studentName = "Aida Millan";
+//let maidenName = "Rocha";
+//let fullName = studentName + maidenName; //complex expression
+//let monthsCoding = 1 * (12 / 6); //complex expression
+//let isStudent = true; //variable expression
+//let onCampus = null; //variable expression
+
+//Step 4 Declare(but do not assign) 9 variables for the following identifiers:First Name, last name, address, city,state,zipcode,your age,referral source, may we contact you
+//let strFirstName;
+//let strLastName;
+//let numAddress;
+//let strCity;
+//let strState;
+//let numZipCode;
+//let numMyAge;
+//let strReferralSource;
+//let strMayWeContactYou;
+
+//Step 5 Demonstrate 3 ways for declaring and assigning values to those variables: (name, city, contact)
+//let firstName = "Aida";
+//let city = "San Diego";
+//let contact = "amillan84@yahoo.com";
+
+//Step 6 Create a variable:Add a number and a string and display the coerced result in the browser’s console window. 
+//let yearsAttending = "one";
+//console.log("I have been taking classes " + yearsAttending + " year");
+
+//step 7 Create 2 variables. 1st variable :Boolean + string and display coerced result repeat for 2nd variable: number and Boolean
+//let petAge = 10;
+//let hisName = "Mr.Blue is";
+//let isCat = petAge == hisName; 
+//console.log("Mr. Blue is a cat " + petAge + "years" + isCat);
+
+//step 8 Is the following string literal? if not how would you fix it?
+//let someString = 'who once said, "Only two things are infinite, the universe and human stupidity", and "Im not sure about the former"';
+//window.console.log(someString);
+
+//step 9 Create a variable that produces a null value in the console window. Then, create a variable that produces an undefined value in the console window.
+ //let total = 90;
+ //total = null;
+ //let priceDisplay;
+ //window.console.log(total);
+
+ //step 10 Use the unary typeof operator on various literals string, number, Boolean, object, undefined
+ //let note1 = "music";
+ //let note2 = 4;
+ //let note3 = false;
+ //let note4 = null;
+ //let note5;
+ //console.log(typeof note1);
+ //console.log(typeof note2);
+ //console.log(typeof note3);
+ //console.log(typeof note4);
+ //console.log(typeof note5);
+
+//step11 Within an alert box, use the concatenation operator (+) to display text in the alert box that appears as follows: 
+//alert("Hello " + "Aida Millan, " + "Welcome to the JavaScript class!");
+ 
+//step 12 Declare a variable called name and set it equal to your name. 
+//let name = aidaMillan;
+//alert("name");
+
+//step 13 Declare a variable course and set it equal to "JavaScript"
+//let course = "JavaScript";
+//alert(course);
+
+//step 14 Re-work the string so that a line break is added right before the word welcome
+//alert("Hello " + "Aida Millan.\nWelcome to the JavaScript class!");
+
+//step 15 Replace your name string with prompt to ask user their name
+//let yourName = prompt('What is your name?');
+//console.log(yourName);
+
+//step 16 Replace class name with a prompt asking user what class are they taking
+//let courseName = prompt('What class are you taking?');
+//console.log(courseName);
+
+//step 17 Declare a variable called x and assign it value of 10, assign y value 20
+//let x = 10;
+//let y = 20;
+//console.log(x += y);
+
+//step 18 Declare variable x and assign value of 20 add and assign 20, console window displays 40
+//let x = 20; 
+//x += 20;
+//console.log(x);
+
+//step 19 Declare variable x assign it 20 multiply and assign 5 to that variable diplays in console window as 100
+//let x = 20;
+//x *= 5;
+//console.log(x);
+
+//step 20 Declare a variable called x and assign it a value that equals the remainder of 20 divided by 3. Divide and assign 1 to that variable.
+//let x = (20 % 3) / 1;
+//console.log(x);
+
+//step 21 Using a set of Comparison and Logical operators, write an application that evaluates to true and displays the result within the console window
+//comparison operator
+//let x = 50;
+//let y = 50;
+//console.log(x <= y);
+//logical operator
+//let x = 10; 
+//let y = 10;
+//console.log(x == 10 || y == 10);
+
+//step 22 Using a set of Comparison and Logical operators, write an application that evaluates to false and displays the result within the console window. The application cannot use the same operators used in the previous application.
+//comparison operator
+//let x = 15;
+//let y = 25;
+//console.log(x == y);
+//logical operator
+//let x = 5;
+//let y = 10;
+//console.log( x ==5 && y<=5);
+
+
